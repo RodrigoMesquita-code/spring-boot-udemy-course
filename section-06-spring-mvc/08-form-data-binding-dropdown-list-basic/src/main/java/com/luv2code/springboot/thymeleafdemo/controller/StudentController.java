053f5590bd2769f08e90c4b105a20ce0e7a8931a
RodrigoMesquita-code <mesquita.rodrigo2102@gmail.com>
2026-06-25 18:12:26 -0300

    Adicionando scripts finais do modulo 5 referentes a spring security e os scripts do modulo 6 sobre Spring MVC, Thymeleaf e Data Binding com formulários

---
+++ section-06-spring-mvc/08-form-data-binding-dropdown-list-basic/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+
+        // create a new student object
+        Student theStudent = new Student();
+
+
+        // add the student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        // log the inpout data
+        System.out.println("theStudent: " + theStudent.getFirstName());
+
+        return "student-confirmation";
+    }
+
+}
---
+++ section-06-spring-mvc/11-form-data-binding-radio-buttons-read-from-props-file/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,50 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+
+        // create a new student object
+        Student theStudent = new Student();
+
+
+        // add the student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
+        // add the list of languages tp the model
+        theModel.addAttribute("languages", languages);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        // log the inpout data
+        System.out.println("theStudent: " + theStudent.getFirstName());
+
+        return "student-confirmation";
+    }
+
+}
