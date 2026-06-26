2597bf33a0dfc08a87623615845dcd8003adaac0
RodrigoMesquita-code <mesquita.rodrigo2102@gmail.com>
2026-06-26 17:56:31 -0300

    Adicionando scripts finais do modulo 6 e o modulo 7 por inteiro, referente a Spring MVC Crud

---
+++ section-04-rest-crud-apis/25-spring-boot-rest-crud-employee-rest-controller-add-employee/src/main/java/com/luv2code/springboot/thymeleafdemo/rest/EmployeeRestController.java
@@ -0,0 +1,52 @@
+package com.luv2code.springboot.thymeleafdemo.rest;
+
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    // quick and dirty: inject employee dao
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findaAll(){
+        return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+            return theEmployee;
+        }
+        // add mapping for POST /employees - add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        // also just in case they pass an is in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+}
--- section-05-rest-api-security/02-spring-boot-rest-security-basic/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ section-05-rest-api-security/02-spring-boot-rest-security-basic/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.luv2code.springboot.cruddemo.security;
+package com.luv2code.springboot.thymeleafdemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
