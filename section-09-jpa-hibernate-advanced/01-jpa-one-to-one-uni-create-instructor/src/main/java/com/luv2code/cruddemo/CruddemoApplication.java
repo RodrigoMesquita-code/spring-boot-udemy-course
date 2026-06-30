83f05adbd40047d98eea4ccaaea24a6d6cf29e4f
RodrigoMesquita-code <mesquita.rodrigo2102@gmail.com>
2026-06-30 17:15:43 -0300

    Adicionando scripts do modulo 8 e do modulo 9 JPA/Hibernate Advanced Mappings

---
+++ section-09-jpa-hibernate-advanced/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,55 @@
+package com.luv2code.cruddemo;
+
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+
+		return runner -> {
+			createInstructor(appDAO);
+		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		/*
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+
+		// create the Intructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube","Luv2code!!");
+		*/
+
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Madhu", "Patel", "mahdu@luv2code.com");
+
+		// create the Intructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube","Guitar");
+
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the Instructor
+		//
+		// NOTE: this will also save the details object because of CascadeType.ALL
+		//
+		System.out.println("Saving Instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done");
+	}
+
+}
